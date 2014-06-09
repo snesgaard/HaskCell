23b502f4febe4dd8b84fc8d0f60777e5466c473d
Sebastian Nesgaard Jensen <s.nesgaard@gmail.com>
2014-06-09 16:14:49 +0200

    Refactored covariance and mean algorithm to used a HMatrix for linear algebra

--- Classifier/Utilities.hs
+++ Classifier/Utilities.hs
@@ -1,37 +1,37 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Utilities where
 
 import Prelude
-import Numeric.Matrix
+import Data.Packed.Matrix
+import Data.Packed.Vector
+import Numeric.LinearAlgebra.Util
+import Numeric.Container
+import Foreign.Storable
 import qualified Data.Map as Map
 
 type ClassTag = Int
 
-appendSample :: MatrixElement a => (ClassTag, Matrix a) -> Map.Map ClassTag (Matrix a) -> Map.Map ClassTag (Matrix a)
-appendSample (c, mc) m = 
-  let sm = maybe mc (addRow mc) $ Map.lookup c m
+appendSample :: (Element a) => (ClassTag, Vector a) -> Map.Map ClassTag (Matrix a) -> Map.Map ClassTag (Matrix a)
+appendSample (c, v) m = 
+  let sm = maybe (asRow v) (addRow v) $ Map.lookup c m
   in Map.insert c sm m
   where
-    addRow a b = a <-> b
+    addRow a b = fromRows $ a:(toRows b)
 
---The number of rows in the matrix must match the number of entries in the class t
---This function divies a given list of ClassTags 
-divideSamples :: MatrixElement a => [(ClassTag, Matrix a)] -> Map.Map ClassTag (Matrix a)
-divideSamples = foldr appendSample Map.empty
 
 -- Accepts a list of mx1 matrix of data and outputs a mxm covariance matrix estimated using the input
-covariance :: (MatrixElement a, Fractional a) => [Matrix a] -> Matrix a
-covariance [] = error "No samples provided"
-covariance (s:sl) =
-  let u = mean (s:sl)
-  in scale (foldr (\ x sc -> sc + (x - u) * transpose (x - u)) z (s:sl)) sn
-  where
-    dim = numRows s
-    n = fromIntegral $ length (s:sl)
-    sn = fromRational $ (1.0 / n)
-    z = matrix (dim, dim) (\(i,j) -> 0)
+covariance :: Matrix Double -> Matrix Double
+covariance m =
+  let r = toRows m
+      u = mean r
+      n = fromIntegral $ rows m
+  in (sum $ map (\v -> outer v v) $ map (\v -> v - u) r) / n
 
-mean :: (MatrixElement a, Fractional a) => [Matrix a] -> Matrix a
-mean (s:sl) = scale (foldr (\x mx -> mx + x) s sl) b
-  where
-    n = fromIntegral $ length $ s:sl
-    b = fromRational $ 1.0 / n
+
+--It is assumed that the dimensionality of each vector is identical
+mean :: [Vector Double] -> Vector Double
+mean v = (sum v) / (fromIntegral $ length v)
+
+vecInt2Frac :: (Integral a, Element a, Fractional b, Element b) => Vector a -> Vector b
+vecInt2Frac = mapVector (\i -> fromIntegral i)
